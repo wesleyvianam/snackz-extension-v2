f18b607c7c5330b4e1af22e7f1fee49beead72f5
wesley martins <wesley.vmartins.js@gmail.com>
2023-12-04 06:52:11 -0300

    List of options in progress

--- src/scripts/getOptions.js
+++ src/scripts/getOptions.js
@@ -1,1 +1,18 @@
-[]
+
+async function fetchCategorias() {
+    try {
+        const response = await fetch('http://127.0.0.1:8000/api/orders', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        console.log(response);
+        const data = await response.json();
+        this.categorias = data; // Assumindo que a resposta é um array de objetos com a propriedade "nome"
+    } catch (error) {
+        console.error('Erro ao buscar categorias:', error);
+    }
+}
+
+fetchCategorias()
